72a54feb6e15438f23fcf371edec7ebf8a975ccf
Phani Katakam <68803561+Phanikatakam@users.noreply.github.com>
2021-08-21 20:04:03 +0530

    Update Greeter.java

--- src/main/java/hello/Greeter.java
+++ src/main/java/hello/Greeter.java
@@ -1,7 +1,7 @@
 package hello;
 
-//public class Greeter {
-  public String sayHello() {
+public class Greeter {
+  //public String sayHello() {
     return "Hello world----- phani is your Trainer ! This is for CICD demo ,Need Bye string to pass the test so Bye";
   }
 }
